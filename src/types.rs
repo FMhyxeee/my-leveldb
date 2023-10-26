553bd92ec35f5ba76f01f04a6fe868e0581d16a3
hyx <hanyuxuan@longi.com>
2023-10-26 09:57:31 +0800

    Implement BlockHandle

--- src/types.rs
+++ src/types.rs
@@ -2,6 +2,8 @@
 
 use std::cmp::Ordering;
 
+use integer_encoding::VarInt;
+
 pub enum ValueType {
     TypeDeletion = 0,
     TypeValue = 1,
@@ -94,5 +96,68 @@ impl Default for ReadOptions {
             fill_cache: true,
             snapshot: None,
         }
+    }
+}
+
+pub struct BlockHandle {
+    offset: usize,
+    size: usize,
+}
+
+impl BlockHandle {
+    /// Decodes a block handle from `from` and returns a block handle
+    /// together with how many bytes we read from the slice.
+    pub fn decode(from: &[u8]) -> (BlockHandle, usize) {
+        let (off, offsize) = usize::decode_var(from).unwrap();
+        let (sz, szsize) = usize::decode_var(&from[offsize..]).unwrap();
+
+        (
+            BlockHandle {
+                offset: off,
+                size: sz,
+            },
+            offsize + szsize,
+        )
+    }
+
+    pub fn new(offset: usize, size: usize) -> BlockHandle {
+        BlockHandle { offset, size }
+    }
+
+    pub fn offset(&self) -> usize {
+        self.offset
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Returns how many bytes were written, or 0 if the write failed because `dst` is too small.
+    pub fn encode_to(&self, dst: &mut [u8]) -> usize {
+        if dst.len() < self.offset.required_space() + self.size.required_space() {
+            0
+        } else {
+            let off = self.offset.encode_var(dst);
+            off + self.size.encode_var(&mut dst[off..])
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_blockhandle() {
+        let hb = BlockHandle::new(890, 777);
+        let mut dst = [0u8; 128];
+
+        let enc_sz = hb.encode_to(&mut dst[..]);
+
+        let (hb2, dec_sz) = BlockHandle::decode(&dst);
+
+        assert_eq!(enc_sz, dec_sz);
+        assert_eq!(hb.size(), hb2.size());
+        assert_eq!(hb.offset(), hb2.offset());
     }
 }
