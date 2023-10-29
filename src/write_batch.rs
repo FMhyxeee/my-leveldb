52b19f4048624821d0795bd34de3e2bdfa9fd273
hyx <1179570770@qq.com>
2023-10-29 17:16:19 +0800

    Add encoding functionality to WriteBatch

--- src/write_batch.rs
+++ src/write_batch.rs
@@ -1,3 +1,5 @@
+use integer_encoding::VarInt;
+
 use crate::{
     memtable::MemTable,
     types::{SequenceNumber, ValueType},
@@ -44,6 +46,23 @@ impl<'a> WriteBatch<'a> {
         self.entries.clear()
     }
 
+    fn byte_size(&self) -> usize {
+        let mut size = 0;
+
+        for e in self.entries.iter() {
+            size += e.key.len() + e.key.len().required_space();
+
+            if let Some(v) = e.val {
+                size += v.len() + v.len().required_space();
+            } else {
+                size += 1;
+            }
+
+            size += 1; // account for tag
+        }
+        size
+    }
+
     fn iter<'b>(&'b self) -> WriteBatchIter<'b, 'a> {
         WriteBatchIter { batch: self, ix: 0 }
     }
@@ -58,6 +77,46 @@ impl<'a> WriteBatch<'a> {
             }
             sequence_num += 1;
         }
+    }
+
+    fn encode(&self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(self.byte_size());
+        let mut ix = 0;
+
+        for (k, v) in self.iter() {
+            if v.is_some() {
+                buf.push(ValueType::TypeValue as u8);
+            } else {
+                buf.push(ValueType::TypeDeletion as u8);
+            }
+
+            ix += 1;
+
+            let req = k.len().required_space();
+            buf.resize(ix + req, 0);
+            ix += k.len().encode_var(&mut buf[ix..ix + req]);
+
+            buf.extend_from_slice(k);
+            ix += k.len();
+
+            let req2;
+            let v_;
+
+            if let Some(v__) = v {
+                v_ = v__;
+                req2 = v_.len().required_space();
+            } else {
+                v_ = "".as_bytes();
+                req2 = 0.required_space();
+            }
+
+            buf.resize(ix + req2, 0);
+            ix += v_.len().encode_var(&mut buf[ix..ix + req2]);
+
+            buf.extend_from_slice(v_);
+            ix += v_.len();
+        }
+        buf
     }
 }
 
@@ -79,6 +138,45 @@ impl<'b, 'a: 'b> Iterator for WriteBatchIter<'b, 'a> {
             ))
         } else {
             None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::WriteBatch;
+
+    #[test]
+    fn test_write_betch() {
+        let mut b = WriteBatch::with_capacity(1, 16);
+
+        let entries = vec![
+            ("abc".as_bytes(), "def".as_bytes()),
+            ("123".as_bytes(), "456".as_bytes()),
+            ("xxx".as_bytes(), "yyy".as_bytes()),
+            ("zzz".as_bytes(), "".as_bytes()),
+            ("010".as_bytes(), "".as_bytes()),
+        ];
+
+        for &(k, v) in entries.iter() {
+            if !v.is_empty() {
+                b.put(k, v);
+            } else {
+                b.delete(k)
+            }
+        }
+
+        assert_eq!(b.byte_size(), 39);
+        assert_eq!(b.encode().len(), 39);
+        assert_eq!(b.iter().count(), 5);
+
+        for (i, (k, v)) in b.iter().enumerate() {
+            assert_eq!(k, entries[i].0);
+
+            match v {
+                None => assert!(entries[i].1.is_empty()),
+                Some(v_) => assert_eq!(v_, entries[i].1),
+            }
         }
     }
 }
