1b46db7505967e8cf75296dd636ea5a199cfa4a0
hyx <1179570770@qq.com>
2023-10-31 22:32:18 +0800

    remove dead_code derive

--- src/blockhandle.rs
+++ src/blockhandle.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 use integer_encoding::VarInt;
 
 pub struct BlockHandle {
--- src/filter_block.rs
+++ src/filter_block.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use crate::filter::FilterPolicy;
 
 use integer_encoding::FixedInt;
