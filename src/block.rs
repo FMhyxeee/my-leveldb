43a495a20abe8f9ccf9d5b92800e7e4d816b1f6c
hyx <1179570770@qq.com>
2023-11-26 20:48:03 +0800

    Implement and test seek_to_last for BlockIter

--- src/block.rs
+++ src/block.rs
@@ -29,7 +29,6 @@ pub struct BlockIter<C: Comparator> {
     offset: usize,
     // start of restarts area
     restarts_off: usize,
-    // start of current entry
     current_entry_offset: usize,
     // tracks the last restart we encountered
     current_restart_ix: usize,
@@ -113,6 +112,21 @@ impl<C: Comparator> BlockIter<C> {
         self.key
             .extend_from_slice(&self.block[self.offset..self.offset + non_shared]);
     }
+
+    pub fn seek_to_last(&mut self) {
+        if self.number_restarts() > 0 {
+            let restart = self.get_restart_point(self.number_restarts() - 1);
+            self.offset = restart;
+            self.current_restart_ix = self.number_restarts() - 1;
+            self.current_entry_offset = restart;
+        } else {
+            self.reset();
+        }
+
+        for (_, _) in self.by_ref() {}
+
+        self.prev();
+    }
 }
 
 impl<C: Comparator> Iterator for BlockIter<C> {
@@ -172,7 +186,7 @@ impl<C: Comparator> LdbIterator for BlockIter<C> {
 
         loop {
             result = self.next();
-            println!("next! {:?}", (self.current_entry_offset, self.offset));
+            // println!("next! {:?}", (self.current_entry_offset, self.offset));
 
             if self.offset >= current_offset {
                 break;
@@ -498,5 +512,40 @@ mod tests {
             block.current(),
             Some(("key1".as_bytes().to_vec(), "value1".as_bytes().to_vec()))
         );
+    }
+
+    #[test]
+    fn test_block_seek_to_last() {
+        let mut o = Options::default();
+
+        // Test with different numbers of restarts
+
+        for block_restart_interval in &[2, 6, 10] {
+            o.block_restart_interval = *block_restart_interval;
+
+            let data = get_data();
+
+            let mut builder = BlockBuilder::new(o, StandardComparator);
+
+            for &(k, v) in data.iter() {
+                builder.add(k, v);
+            }
+
+            let block_contents = builder.finish();
+
+            let mut block = BlockIter::new(block_contents, StandardComparator);
+
+            block.seek_to_last();
+
+            assert!(block.valid());
+
+            assert_eq!(
+                block.current(),
+                Some((
+                    "prefix_key3".as_bytes().to_vec(),
+                    "value".as_bytes().to_vec()
+                ))
+            );
+        }
     }
 }
