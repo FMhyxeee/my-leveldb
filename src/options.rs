3e7c35bf4bd166545e99a4e222c3223044e81712
hyx <1179570770@qq.com>
2023-12-25 21:28:56 +0800

    Enable checksum verification by default

--- src/options.rs
+++ src/options.rs
@@ -96,7 +96,7 @@ pub struct ReadOptions {
 impl Default for ReadOptions {
     fn default() -> Self {
         Self {
-            verify_checksums: false,
+            verify_checksums: true,
             fill_cache: true,
             snapshot: None,
         }
