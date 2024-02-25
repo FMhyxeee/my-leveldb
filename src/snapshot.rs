29cc0aad7dd690603307c698cb4b5dfbd0514ebe
hyx <1179570770@qq.com>
2024-02-25 15:24:14 +0800

    snapshot: Add method to retrieve sequence number for a snapshot.

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -31,6 +31,10 @@ impl SnapshotList {
         self.newest
     }
 
+    pub fn sequence_at(&self, ss: &Snapshot) -> Option<SequenceNumber> {
+        self.map.get(ss).copied()
+    }
+
     pub fn oldest(&self) -> SequenceNumber {
         *self.map.get(&self.oldest).unwrap()
     }
