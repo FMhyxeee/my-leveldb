6a04766aebe3e8279cf3bd8725050886fb01ca6a
hyx <hanyuxuan@longi.com>
2024-04-02 15:53:32 +0800

    Remove useless logging statement.

--- src/db_impl.rs
+++ src/db_impl.rs
@@ -293,7 +293,6 @@ impl DB {
         let filenames = self.opt.env.children(Path::new(&self.name))?;
         for name in filenames {
             if let Ok((num, typ)) = parse_file_name(&name) {
-                log!(self.opt.log, "{} {:?}", num, typ);
                 match typ {
                     FileType::Log => {
                         if num >= self.vset.borrow().log_num {
