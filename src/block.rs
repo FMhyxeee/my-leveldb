ed7fb02e5e0f4c921c99b03f394d8d12833e6d2f
hyx <hanyuxuan@longi.com>
2024-07-18 16:11:43 +0800

    create block iter.

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,10 @@
+use std::cmp::Ordering;
+
+use crate::types::LdbIterator;
 use crate::{types::StandardComparator, Comparator};
 
 use integer_encoding::FixedInt;
+use integer_encoding::VarInt;
 
 pub type BlockContents = Vec<u8>;
 
@@ -54,13 +58,103 @@ impl<C: Comparator> Block<C> {
         usize::decode_fixed(&self.data[restart..restart + 4]).unwrap()
     }
 
-    // pub fn iter<'a>(&'a self) -> BlockIter<'a, C> {
-    //     BlockIter {
-    //         block: self,
-    //         current_restart_ix: 0,
-    //         offset: 0,
-    //         key: Vec::new(),
-    //         val_offset: 0,
-    //     }
-    // }
+    pub fn iter(&self) -> BlockIter<C> {
+        BlockIter {
+            block: self,
+            current_restart_ix: 0,
+            offset: 0,
+            key: Vec::new(),
+            val_offset: 0,
+        }
+    }
+}
+
+pub struct BlockIter<'a, C: 'a + Comparator> {
+    block: &'a Block<C>,
+    offset: usize,
+    current_restart_ix: usize,
+
+    // We assemble the key from two parts usually, so we keep the current full key here.
+    key: Vec<u8>,
+    val_offset: usize,
+}
+
+impl<'a, C: Comparator> BlockIter<'a, C> {
+    // Returns SHARED, NON_SHARED and VALSIZE from the current position. Advances self.offset.
+    fn parse_entry(&mut self) -> (usize, usize, usize) {
+        let mut i = 0;
+        let (shared, sharedlen) = usize::decode_var(&self.block.data[self.offset..]).unwrap();
+        i += sharedlen;
+
+        let (non_shared, non_sharedlen) =
+            usize::decode_var(&self.block.data[self.offset + i..]).unwrap();
+        i += non_sharedlen;
+
+        let (valsize, valsize_len) =
+            usize::decode_var(&self.block.data[self.offset + i..]).unwrap();
+        i += valsize_len;
+
+        self.offset += i;
+        (shared, non_shared, valsize)
+    }
+
+    /// offset is assumed to be at the beginning of the non-shared key part.
+    /// offset is not advanced.
+    fn assemble_key(&mut self, shared: usize, non_shared: usize) {
+        self.key.resize(shared, 0);
+        self.key
+            .extend_from_slice(&self.block.data[self.offset..self.offset + non_shared]);
+    }
+}
+
+impl<'a, C: Comparator> Iterator for BlockIter<'a, C> {
+    // This is ugly, but necessary because of Iterator's signature
+    type Item = (Vec<u8>, &'a [u8]);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current_offset = self.offset;
+
+        if current_offset >= self.block.restarts_off {
+            return None;
+        }
+        let (shared, non_shared, valsize) = self.parse_entry();
+        self.assemble_key(shared, non_shared);
+
+        self.val_offset = self.offset + non_shared;
+        self.offset = self.val_offset + valsize;
+
+        let num_restarts = self.block.number_restarts();
+        while self.current_restart_ix + 1 < num_restarts
+            && self.block.get_restart_point(self.current_restart_ix + 1) < current_offset
+        {
+            self.current_restart_ix += 1;
+        }
+        Some((
+            self.key.clone(),
+            &self.block.data[self.val_offset..self.val_offset + valsize],
+        ))
+    }
+}
+
+impl<'a, C: 'a + Comparator> LdbIterator<'a> for BlockIter<'a, C> {
+    // TODO: Use binary search here
+    fn seek(&mut self, to: &[u8]) {
+        for (k, _) in self.by_ref() {
+            if C::cmp(k.as_slice(), to) != Ordering::Less {
+                break;
+            }
+        }
+    }
+
+    fn valid(&self) -> bool {
+        !self.key.is_empty() && self.val_offset > 0 && self.val_offset < self.block.restarts_off
+    }
+
+    fn current(&self) -> Self::Item {
+        assert!(self.valid());
+        (
+            self.key.clone(),
+            &self.block.data[self.val_offset..self.offset],
+        )
+    }
 }
