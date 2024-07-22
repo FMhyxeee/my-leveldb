fd9326ce1f19fd9b4312d2a8b7946d589f0cfcc7
hyx <hanyuxuan@longi.com>
2024-07-22 16:28:41 +0800

    Optimize LookupKey allocation and add tests.

--- src/memtable.rs
+++ src/memtable.rs
@@ -20,6 +20,8 @@ impl LookupKey {
         );
 
         let mut i = 0;
+        key.reserve(8 + k.len().required_space() + k.len());
+
         key.resize(k.len().required_space(), 0);
         i = k.len().encode_var(&mut key[i..]);
 
@@ -249,6 +251,25 @@ mod tests {
             mt.add(seq, ValueType::TypeValue, k.as_bytes(), v.as_bytes());
         }
         mt
+    }
+
+    #[test]
+    fn test_lookupkey() {
+        use integer_encoding::VarInt;
+
+        let lk1 = LookupKey::new(b"abcde", 123);
+        let lk2 = LookupKey::new(b"xyabxy", 97);
+
+        // Assert correct allocation strategy
+        assert_eq!(lk1.key.len(), 14);
+        assert_eq!(lk1.key.capacity(), 14);
+
+        assert_eq!(lk1.user_key(), "abcde".as_bytes());
+        assert_eq!(u32::decode_var(lk1.memtable_key()).unwrap(), (5, 1));
+        assert_eq!(
+            lk2.internal_key(),
+            vec![120, 121, 97, 98, 120, 121, 1, 97, 0, 0, 0, 0, 0, 0].as_slice()
+        );
     }
 
     #[test]
