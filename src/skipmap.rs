4085bc42f011c5f6c6dadd7ee2a6038b43124b83
hyx <hanyuxuan@longi.com>
2024-07-31 14:50:47 +0800

    Fix comment type of some comments.

--- src/skipmap.rs
+++ src/skipmap.rs
@@ -77,7 +77,7 @@ impl<C: Comparator> SkipMap<C> {
         n.key.starts_with(key)
     }
 
-    //Return the node with key or the next greater one.
+    /// Return the node with key or the next greater one.
     fn get_greater_or_equal(&self, key: &[u8]) -> &Node {
         // Start at the highest skip link of the head node, and work down from there
         let mut current: *const Node = unsafe { transmute_copy(&self.head.as_ref()) };
@@ -225,7 +225,7 @@ impl<C: Comparator> SkipMap<C> {
         }
     }
 
-    // Runs through the skipmap and prints everything including addresses
+    /// Runs through the skipmap and prints everything including addresses
     fn dbg_print(&self) {
         let mut current: *const Node = &*self.head;
         loop {
