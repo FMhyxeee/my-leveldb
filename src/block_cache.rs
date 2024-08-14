a96fbac238c536521c4abca63082027c38a8b3ca
hyx <hanyuxuan@longi.com>
2024-08-14 11:28:10 +0800

    Implement Cache.

--- src/block_cache.rs
+++ src/block_cache.rs
@@ -3,8 +3,7 @@ use std::{
     mem::{swap, transmute_copy},
 };
 
-use crate::block::BlockContents;
-
+// No clone, no copy! The asserts that an LRUHandle exists only once.
 type LRUHandle<T> = *mut LRUNode<T>;
 
 struct LRUNode<T> {
@@ -84,6 +83,24 @@ impl<T> LRUList<T> {
         }
     }
 
+    fn remove(&mut self, node_handle: LRUHandle<T>) -> T {
+        unsafe {
+            // If has next
+            if let Some(ref mut nextp) = (*node_handle).next {
+                swap(&mut nextp.prev, &mut (*node_handle).prev);
+            }
+            // If has prev
+            if let Some(ref mut prevp) = (*node_handle).prev {
+                swap(&mut (**prevp).next, &mut (*node_handle).next);
+            }
+
+            self.count -= 1;
+            // node_handle now only has references/objects that point to itself,
+            // so it's safe to drop
+            (*node_handle).data.take().unwrap() as T
+        }
+    }
+
     /// Reinserts the referenced node at the front.
     fn reinsert_front(&mut self, node_handle: LRUHandle<T>) {
         unsafe {
@@ -131,19 +148,156 @@ impl<T> LRUList<T> {
     }
 }
 
-type Cachehandle = usize;
+pub type CacheKey = Vec<u8>;
+type CacheEntry<T> = (T, LRUHandle<CacheKey>);
 
 /// Implementation of `SharedLRUCache`.
 /// Based on a HashMap; the elements are linked in order to support the LRU ordering.
-pub struct BlockCache {
-    list: LRUList<Cachehandle>,
-    map: HashMap<Cachehandle, BlockContents>,
-    handle_counter: Cachehandle,
+pub struct Cache<T> {
+    // note: CacheKeys (Vec<u8>) are duplicated between list and map. If this turns out to be a
+    // performance bottleneck, another layer of indirection can solve this by mapping the key to a
+    // numeric handle that keys both list and map.
+    list: LRUList<CacheKey>,
+    map: HashMap<CacheKey, CacheEntry<T>>,
+    cap: usize,
+}
+
+impl<T> Cache<T> {
+    pub fn new(capacity: usize) -> Self {
+        assert!(capacity > 0);
+        Self {
+            list: LRUList::new(),
+            map: HashMap::new(),
+            cap: capacity,
+        }
+    }
+
+    /// How many the cache currently contains
+    pub fn count(&self) -> usize {
+        self.list.count()
+    }
+
+    /// The capacity of this cache
+    pub fn cap(&self) -> usize {
+        self.cap
+    }
+
+    /// Insert a new element into the cache. The returned `CacheHandle` can be used for further
+    /// operations on the element.
+    /// If the capacity has been reached, The least recently used element is removed from the
+    /// cache.
+    pub fn insert(&mut self, key: &CacheKey, elem: T) {
+        if self.list.count() >= self.cap {
+            if let Some(removed_key) = self.list.remove_last() {
+                assert!(self.map.remove(&removed_key).is_some());
+            } else {
+                panic!("could not remove_last(); bug!");
+            }
+        }
+
+        let lru_handle = self.list.insert(key.clone());
+        self.map.insert(key.clone(), (elem, lru_handle));
+    }
+
+    /// Retrieve an element from the cache.
+    /// If the element has been preempted from the cache in the meantime, this returns None.
+    pub fn get(&mut self, key: &CacheKey) -> Option<&T> {
+        if let Some((elem, lru_handle)) = self.map.get_mut(key) {
+            self.list.reinsert_front(*lru_handle);
+            Some(elem)
+        } else {
+            None
+        }
+    }
+
+    /// Remove an element from the cache (for invalidation).
+    pub fn remove(&mut self, key: &CacheKey) -> Option<T> {
+        match self.map.remove(key) {
+            None => None,
+            Some((elem, lru_handle)) => {
+                self.list.remove(lru_handle);
+                Some(elem)
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_blockcache_cache_add_rm() {
+        let mut cache = Cache::new(128);
+
+        let h_123 = "aaa".as_bytes().to_vec();
+        let h_521 = "aab".as_bytes().to_vec();
+        let h_372 = "aac".as_bytes().to_vec();
+        let h_332 = "aad".as_bytes().to_vec();
+        let h_899 = "aae".as_bytes().to_vec();
+
+        cache.insert(&h_123, 123);
+        cache.insert(&h_332, 332);
+        cache.insert(&h_521, 521);
+        cache.insert(&h_372, 372);
+        cache.insert(&h_899, 899);
+
+        assert_eq!(cache.count(), 5);
+
+        assert_eq!(cache.get(&h_123), Some(&123));
+        assert_eq!(cache.get(&h_372), Some(&372));
+
+        assert_eq!(cache.remove(&h_521), Some(521));
+        assert_eq!(cache.get(&h_521), None);
+        assert_eq!(cache.remove(&h_521), None);
+
+        assert_eq!(cache.count(), 4);
+    }
+
+    #[test]
+    fn test_blockcache_cache_capacity() {
+        let mut cache = Cache::new(3);
+
+        let h_123 = "aaa".as_bytes().to_vec();
+        let h_521 = "aab".as_bytes().to_vec();
+        let h_372 = "aac".as_bytes().to_vec();
+        let h_332 = "aad".as_bytes().to_vec();
+        let h_899 = "aae".as_bytes().to_vec();
+
+        cache.insert(&h_123, 123);
+        cache.insert(&h_332, 332);
+        cache.insert(&h_521, 521);
+        cache.insert(&h_372, 372);
+        cache.insert(&h_899, 899);
+
+        assert_eq!(cache.count(), 3);
+
+        assert_eq!(cache.get(&h_123), None);
+        assert_eq!(cache.get(&h_332), None);
+        assert_eq!(cache.get(&h_521), Some(&521));
+        assert_eq!(cache.get(&h_372), Some(&372));
+        assert_eq!(cache.get(&h_899), Some(&899));
+    }
+
+    #[test]
+    fn test_blockcache_lru_remove() {
+        let mut lru = LRUList::<usize>::new();
+
+        let h_56 = lru.insert(56);
+        lru.insert(22);
+        lru.insert(223);
+        let h_244 = lru.insert(244);
+        lru.insert(1111);
+        let h_12 = lru.insert(12);
+
+        assert_eq!(lru.count(), 6);
+        assert_eq!(244, lru.remove(h_244));
+        assert_eq!(lru.count(), 5);
+        assert_eq!(12, lru.remove(h_12));
+        assert_eq!(lru.count(), 4);
+        assert_eq!(56, lru.remove(h_56));
+        assert_eq!(lru.count(), 3);
+    }
 
     #[test]
     fn test_blockcache_lru_1() {
